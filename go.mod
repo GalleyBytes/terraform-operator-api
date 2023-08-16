a107d376503b58d7a7cf1956e071e1b16c94f3e9
isaaguilar <isa@aguilarstory.com>
2023-08-16 09:23:47 -0400

    Added an authenticated debug endpoint
    - Added `/api/v1/cluster/<clustername>/debug/<namespace>/<name>` to the api.
    - Silence the klog logs since they're too verbose and interfere with stdout on the TTY of a debug connection. The connection might be better suited to run in another process.

--- go.mod
+++ go.mod
@@ -11,9 +11,37 @@ require (
 	github.com/spf13/viper v1.12.0
 	gorm.io/driver/postgres v1.3.9
 	gorm.io/gorm v1.23.8
-	k8s.io/api v0.27.3
-	k8s.io/apimachinery v0.27.3
-	k8s.io/client-go v0.27.3
+	k8s.io/api v0.27.4
+	k8s.io/apimachinery v0.27.4
+	k8s.io/client-go v0.27.4
+)
+
+require (
+	github.com/Azure/go-ansiterm v0.0.0-20210617225240-d185dfc1b5a1 // indirect
+	github.com/MakeNowJust/heredoc v1.0.0 // indirect
+	github.com/chai2010/gettext-go v1.0.2 // indirect
+	github.com/evanphx/json-patch v5.6.0+incompatible // indirect
+	github.com/exponent-io/jsonpath v0.0.0-20151013193312-d6023ce2651d // indirect
+	github.com/fatih/camelcase v1.0.0 // indirect
+	github.com/fvbommel/sortorder v1.0.1 // indirect
+	github.com/go-errors/errors v1.4.2 // indirect
+	github.com/google/btree v1.0.1 // indirect
+	github.com/google/shlex v0.0.0-20191202100458-e7afc7fbc510 // indirect
+	github.com/gregjones/httpcache v0.0.0-20180305231024-9cad4c3443a7 // indirect
+	github.com/inconshreveable/mousetrap v1.0.1 // indirect
+	github.com/liggitt/tabwriter v0.0.0-20181228230101-89fcab3d43de // indirect
+	github.com/mitchellh/go-wordwrap v1.0.0 // indirect
+	github.com/moby/spdystream v0.2.0 // indirect
+	github.com/moby/term v0.0.0-20221205130635-1aeaba878587 // indirect
+	github.com/monochromegane/go-gitignore v0.0.0-20200626010858-205db1a8cc00 // indirect
+	github.com/peterbourgon/diskv v2.0.1+incompatible // indirect
+	github.com/russross/blackfriday/v2 v2.1.0 // indirect
+	github.com/spf13/cobra v1.6.0 // indirect
+	github.com/xlab/treeprint v1.1.0 // indirect
+	go.starlark.net v0.0.0-20200306205701-8dd3e2ee1dd5 // indirect
+	k8s.io/component-base v0.27.4 // indirect
+	sigs.k8s.io/kustomize/api v0.13.2 // indirect
+	sigs.k8s.io/kustomize/kyaml v0.14.1 // indirect
 )
 
 // replace github.com/isaaguilar/kedge v0.0.0-20230616161426-17e452296bfc => /tmp/vgo
@@ -32,7 +60,6 @@ require (
 	github.com/gogo/protobuf v1.3.2 // indirect
 	github.com/golang/protobuf v1.5.3 // indirect
 	github.com/google/gnostic v0.6.9 // indirect
-	github.com/google/gnostic-models v0.6.8 // indirect
 	github.com/google/go-cmp v0.5.9 // indirect
 	github.com/google/gofuzz v1.2.0 // indirect
 	github.com/google/uuid v1.3.0 // indirect
@@ -49,9 +76,9 @@ require (
 	golang.org/x/time v0.3.0 // indirect
 	google.golang.org/appengine v1.6.7 // indirect
 	gopkg.in/inf.v0 v0.9.1 // indirect
-	k8s.io/klog/v2 v2.100.1 // indirect
+	k8s.io/klog/v2 v2.100.1
 	// k8s.io/kube-openapi v0.0.0-20230501164219-8b0f38b5fd1f // I WANT THIS ONE
-	k8s.io/kube-openapi v0.0.0-20230501164219-8b0f38b5fd1f
+	k8s.io/kube-openapi v0.0.0-20230501164219-8b0f38b5fd1f // indirect
 	k8s.io/utils v0.0.0-20230505201702-9f6742963106 // indirect
 	sigs.k8s.io/controller-runtime v0.15.0 // indirect
 	sigs.k8s.io/json v0.0.0-20221116044647-bc3834ca7abd // indirect
@@ -60,6 +87,7 @@ require (
 )
 
 require (
+	github.com/creack/pty v1.1.18
 	github.com/fsnotify/fsnotify v1.6.0 // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
 	github.com/go-playground/locales v0.14.0 // indirect
@@ -87,6 +115,7 @@ require (
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/pelletier/go-toml v1.9.5 // indirect
 	github.com/pelletier/go-toml/v2 v2.0.5 // indirect
+	github.com/sorenisanerd/gotty v1.5.0
 	github.com/spf13/afero v1.9.2 // indirect
 	github.com/spf13/cast v1.5.0 // indirect
 	github.com/spf13/jwalterweatherman v1.1.0 // indirect
@@ -101,4 +130,6 @@ require (
 	gopkg.in/ini.v1 v1.67.0 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1
+	k8s.io/cli-runtime v0.27.4
+	k8s.io/kubectl v0.27.4
 )
--- pkg/api/get_record.go
+++ pkg/api/get_record.go
@@ -1,19 +1,35 @@
 package api
 
 import (
+	"encoding/base64"
 	"encoding/json"
 	"errors"
 	"fmt"
+	"io"
+	"io/ioutil"
 	"log"
 	"math"
 	"net/http"
+	"os"
 	"time"
 
+	ptylib "github.com/creack/pty"
 	"github.com/galleybytes/terraform-operator-api/pkg/common/models"
+	tfv1beta1 "github.com/galleybytes/terraform-operator/pkg/apis/tf/v1beta1"
+	tfo "github.com/galleybytes/terraform-operator/pkg/client/clientset/versioned"
 	"github.com/gin-gonic/gin"
 	"github.com/gorilla/websocket"
+	"github.com/sorenisanerd/gotty/webtty"
 	"gopkg.in/yaml.v3"
 	"gorm.io/gorm"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/watch"
+	"k8s.io/cli-runtime/pkg/genericclioptions"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/remotecommand"
+	"k8s.io/kubectl/pkg/cmd/exec"
+	"k8s.io/kubectl/pkg/scheme"
 )
 
 type Response struct {
@@ -612,4 +628,578 @@ func (h APIHandler) ResourceLogWatcher(c *gin.Context) {
 		}
 	}
 
+}
+
+// Check if terraform namespace/name resource exists in vcluster
+func isResourceExist(parentClientset kubernetes.Interface, clusterName, namespace, name string, c *gin.Context) bool {
+	config, err := getVclusterConfig(parentClientset, "internal", clusterName)
+	if err != nil {
+		return false
+	}
+	tfoclientset := tfo.NewForConfigOrDie(config)
+	if _, err := tfoclientset.TfV1beta1().Terraforms(namespace).Get(c, name, metav1.GetOptions{}); err != nil {
+		return false
+	}
+	return true
+}
+
+func (h APIHandler) Debugger(c *gin.Context) {
+	clusterName := c.Param("cluster_name")
+	clusterID := h.getClusterID(clusterName)
+	if clusterID == 0 {
+		c.JSON(http.StatusUnprocessableEntity, response(http.StatusUnprocessableEntity, fmt.Sprintf("cluster_name '%s' not found", clusterName), nil))
+		return
+	}
+	name := c.Param("name")
+	namespace := c.Param("namespace")
+	if !isResourceExist(h.clientset, clusterName, namespace, name, c) {
+		c.JSON(http.StatusUnprocessableEntity, response(http.StatusUnprocessableEntity, fmt.Sprintf("tf resource '%s/%s' not found", namespace, name), nil))
+		return
+	}
+
+	var wsupgrader = websocket.Upgrader{
+		ReadBufferSize:  1024,
+		WriteBufferSize: 1024,
+	}
+	// how can i make this more secure?
+	wsupgrader.CheckOrigin = func(r *http.Request) bool { return true }
+
+	conn, err := wsupgrader.Upgrade(c.Writer, c.Request, nil)
+	if err != nil {
+		log.Printf("Failed to set websocket upgrade: %+v", err)
+		return
+	}
+	defer conn.Close()
+
+	podExecReadWriter, err := New(h.clientset, clusterName, namespace, name, c)
+	if err != nil {
+		log.Printf("Failed to connect to debug pod: %s", err)
+		return
+	}
+
+	var opts []webtty.Option
+	opts = append(opts, webtty.WithPermitWrite())
+
+	webTTY, err := webtty.New(&wsWrapper{conn}, podExecReadWriter, opts...)
+	if err != nil {
+		log.Printf("failed to create webtty: %s", err)
+		return
+	}
+
+	errCh := make(chan error)
+	go func() {
+		errCh <- webTTY.Run(c)
+	}()
+
+	select {
+	case err = <-errCh:
+		log.Println(err)
+		msg := websocket.FormatCloseMessage(websocket.CloseGoingAway, "")
+		err := conn.WriteMessage(websocket.CloseMessage, msg)
+		if err != nil {
+			log.Println(err)
+		}
+	case err = <-podExecReadWriter.Closer:
+		log.Println("Closing the connection")
+		var closeCode int
+		var msg string
+		if err != nil {
+			closeCode = websocket.CloseGoingAway
+			msg = err.Error()
+		} else {
+			closeCode = websocket.CloseNormalClosure
+			msg = ""
+		}
+		closeMsg := websocket.FormatCloseMessage(closeCode, msg)
+		err := conn.WriteMessage(websocket.CloseMessage, closeMsg)
+		if err != nil {
+			log.Println(err)
+		}
+	}
+
+	// return
+}
+
+type wsWrapper struct {
+	*websocket.Conn
+}
+
+func (wsw *wsWrapper) Write(p []byte) (n int, err error) {
+	// log.Println("wsWrapper.Write ", string(p))
+	writer, err := wsw.Conn.NextWriter(websocket.TextMessage)
+	if err != nil {
+		return 0, err
+	}
+	defer writer.Close()
+	return writer.Write(p)
+}
+
+func (wsw *wsWrapper) Read(p []byte) (n int, err error) {
+	// log.Println("wsWrapper.Read ", string(p))
+	for {
+		msgType, reader, err := wsw.Conn.NextReader()
+		if err != nil {
+			// log.Println("I didn't read it cuz err", err)
+			return 0, err
+		}
+
+		if msgType != websocket.TextMessage {
+			// log.Println("I didn't read it cuz wrong type")
+			continue
+		}
+
+		b, err := ioutil.ReadAll(reader)
+		if len(b) > len(p) {
+			// log.Println("I didn't read it cuz another error", err)
+			return 0, fmt.Errorf("client message exceeded buffer size: %s", err)
+		}
+		// log.Println("I did read something into a reader which produced this: %s", string(b))
+
+		// log.Println("I think I read it but I'm not 100%% sure", err)
+		dec, err := base64.StdEncoding.DecodeString(string(b[1:]))
+		if err != nil {
+			// log.Println("I didn't read it cuz could not decode", err)
+			continue
+		}
+		// log.Printf("I'm reading dec = %s ", string(dec))
+
+		n = copy(p, append([]byte{b[0]}, dec...))
+		return n, err
+		// n, err = wsw.podTTY.Write(dec)
+		// if err != nil {
+		// 	return 0, err
+		// }
+
+	}
+}
+
+// type PodExecFactory struct{}
+type PodExec struct {
+	pty       *os.File
+	reader    io.Reader
+	writer    io.Writer
+	termSizer TermSizer
+	Closer    chan error
+}
+
+type TermSizer struct {
+	SizeCh chan remotecommand.TerminalSize
+}
+
+func (t TermSizer) Next() *remotecommand.TerminalSize {
+	size := <-t.SizeCh
+	return &size
+}
+
+// func (f *PodExecFactory) Name() string {
+// 	return "Pod Exec"
+// }
+
+// func (f *PodExecFactory) New(params map[string][]string, headers map[string][]string) (Slave, error) {
+// 	return New()
+// }
+
+// command string, argv []string, headers map[string][]string, options ...Option
+func New(clientset kubernetes.Interface, clusterName, namespace, name string, c *gin.Context) (*PodExec, error) {
+	pty, tty, err := ptylib.Open()
+	if err != nil {
+		log.Fatal(err)
+	}
+	os.Stdin = tty
+	os.Stderr = tty
+	os.Stdout = tty
+
+	sizeCh := make(chan remotecommand.TerminalSize)
+	closeCh := make(chan error)
+
+	termSizer := TermSizer{
+		SizeCh: sizeCh,
+	}
+
+	go func() {
+		defer pty.Close()
+		err := RemoteDebug(clientset, clusterName, namespace, name, tty, c, termSizer)
+		log.Println("Pod exec exited")
+		closeCh <- err
+	}()
+
+	return &PodExec{
+		pty:       pty,
+		reader:    pty,
+		writer:    pty,
+		termSizer: termSizer,
+		Closer:    closeCh,
+	}, nil
+
+}
+
+func (podExec *PodExec) Read(p []byte) (n int, err error) {
+	return podExec.reader.Read(p)
+}
+
+func (podExec *PodExec) Write(p []byte) (n int, err error) {
+	return podExec.writer.Write(p)
+}
+
+// WindowTitleVariables returns any values that can be used to fill out
+// the title of a terminal.
+func (p *PodExec) WindowTitleVariables() map[string]interface{} {
+	return map[string]interface{}{
+		"command":  "tfo",
+		"hostname": "localhost",
+	}
+}
+
+// ResizeTerminal sets a new size of the terminal.
+func (p *PodExec) ResizeTerminal(columns int, rows int) error {
+	// log.Println("Resizing Terminal", columns, rows)
+
+	p.termSizer.SizeCh <- remotecommand.TerminalSize{
+		Width:  uint16(columns),
+		Height: uint16(rows),
+	}
+
+	return nil
+}
+
+func (p *PodExec) Close() error {
+	log.Println("Going away...")
+	return nil
+}
+
+// RemoteDebug starts the debug pod and connects in a tty that will be synced thru a websocket. Anything written to
+// stdout will be synced to the tty. stderr logs will show up in the api logs and not the tty.
+func RemoteDebug(parentClientset kubernetes.Interface, clusterName, namespace, name string, tty *os.File, c *gin.Context, terminalSizeQueue remotecommand.TerminalSizeQueue) error {
+
+	config, err := getVclusterConfig(parentClientset, "internal", clusterName)
+	if err != nil {
+		return err
+	}
+	tfoclientset := tfo.NewForConfigOrDie(config)
+	clientset := kubernetes.NewForConfigOrDie(config)
+	tfclient := tfoclientset.TfV1beta1().Terraforms(namespace)
+	// newSession()
+
+	// tfoclientset := session.tfoclientset.TfV1beta1().Terraforms(namespace)
+	podClient := clientset.CoreV1().Pods(namespace)
+
+	tf, err := tfclient.Get(c, name, metav1.GetOptions{})
+	if err != nil {
+		return err
+	}
+
+	pod := generatePod(tf)
+	pod, err = podClient.Create(c, pod, metav1.CreateOptions{})
+	if err != nil {
+		return err
+	}
+	defer podClient.Delete(c, pod.Name, metav1.DeleteOptions{})
+
+	fmt.Printf("Connecting to %s ", pod.Name)
+
+	watcher, err := podClient.Watch(c, metav1.ListOptions{
+		FieldSelector: "metadata.name=" + pod.Name,
+	})
+	if err != nil {
+		return err
+	}
+
+	for event := range watcher.ResultChan() {
+		fmt.Printf(".")
+		switch event.Type {
+		case watch.Modified:
+			pod = event.Object.(*corev1.Pod)
+			// If the Pod contains a status condition Ready == True, stop
+			// watching.
+			for _, cond := range pod.Status.Conditions {
+				if cond.Type == corev1.PodReady &&
+					cond.Status == corev1.ConditionTrue &&
+					pod.Status.Phase == corev1.PodRunning {
+					watcher.Stop()
+				}
+			}
+		default:
+			// fmt.Fprintln(os.Stderr, event.Type)
+		}
+	}
+
+	// log.Printf("tty isTerminal? %t", isTerminal(tty))
+	ioStreams := genericclioptions.IOStreams{In: tty, Out: tty, ErrOut: tty}
+	// log.Println("Setting up steamOptions")
+	streamOptions := exec.StreamOptions{
+		IOStreams: ioStreams,
+		Stdin:     true,
+		TTY:       true,
+	}
+	// log.Println("Checking TTY setup")
+	t := streamOptions.SetupTTY()
+	if t.Raw {
+		// unset p.Err if it was previously set because both stdout and stderr go over p.Out when tty is
+		// true
+		streamOptions.ErrOut = nil
+	}
+	// log.Println(file.Name())
+	// log.Println("Setting up request")
+
+	fn := func() error {
+		req := clientset.CoreV1().RESTClient().
+			Post().
+			Namespace(pod.Namespace).
+			Resource("pods").
+			Name(pod.Name).
+			SubResource("exec").
+			VersionedParams(&corev1.PodExecOptions{
+				Container: pod.Spec.Containers[0].Name,
+				Command: []string{
+					"/bin/bash",
+					"-c",
+					"cd $TFO_MAIN_MODULE && export PS1=\"\\w\\$ \" && " +
+						"if [[ -n \"$AWS_WEB_IDENTITY_TOKEN_FILE\" ]];then " +
+						"export $(irsa-tokengen); " +
+						"echo printf \"\nAWS creds set from token file\n\";fi &&" +
+						"printf \"\nTry running 'terraform init'\n\n\" && bash",
+				},
+				// Stdin:  streamOptions.Stdin,
+				// Stdout: streamOptions.Out != nil,
+				// Stderr: streamOptions.ErrOut != nil,
+				Stdin:  true,
+				Stdout: true,
+				Stderr: true,
+				TTY:    t.Raw,
+			}, scheme.ParameterCodec)
+
+		return func() error {
+
+			exec, err := remotecommand.NewSPDYExecutor(config, "POST", req.URL())
+			if err != nil {
+				return err
+			}
+
+			return exec.StreamWithContext(c, remotecommand.StreamOptions{
+				Stdin:             streamOptions.In,
+				Stdout:            streamOptions.Out,
+				Stderr:            streamOptions.ErrOut,
+				Tty:               t.Raw,
+				TerminalSizeQueue: terminalSizeQueue,
+			})
+		}()
+
+	}
+
+	if err := t.Safe(fn); err != nil {
+		return err
+	}
+	return nil
+}
+
+// func isTerminal(file *os.File) bool {
+
+// 	inFd := file.Fd()
+// 	log.Println(file.Name(), inFd)
+// 	_, err := unix.IoctlGetTermios(int(inFd), unix.TIOCGETA)
+// 	return err == nil
+
+// }
+
+func generatePod(tf *tfv1beta1.Terraform) *corev1.Pod {
+	terraformVersion := tf.Spec.TerraformVersion
+	if terraformVersion == "" {
+		terraformVersion = "1.1.5"
+	}
+	generation := fmt.Sprint(tf.Generation)
+	versionedName := tf.Status.PodNamePrefix + "-v" + generation
+	generateName := versionedName + "-debug-"
+	generationPath := "/home/tfo-runner/generations/" + generation
+	env := []corev1.EnvVar{}
+	envFrom := []corev1.EnvFromSource{}
+	annotations := make(map[string]string)
+	labels := make(map[string]string)
+	for _, taskOption := range tf.Spec.TaskOptions {
+		if tfv1beta1.ListContainsTask(taskOption.For, "*") {
+			env = append(env, taskOption.Env...)
+			envFrom = append(envFrom, taskOption.EnvFrom...)
+			for key, value := range taskOption.Annotations {
+				annotations[key] = value
+			}
+			for key, value := range taskOption.Labels {
+				labels[key] = value
+			}
+		}
+	}
+	env = append(env, []corev1.EnvVar{
+		{
+			Name:  "TFO_TASK",
+			Value: "debug",
+		},
+		{
+			Name:  "TFO_RESOURCE",
+			Value: tf.Name,
+		},
+		{
+			Name:  "TFO_NAMESPACE",
+			Value: tf.Namespace,
+		},
+		{
+			Name:  "TFO_GENERATION",
+			Value: generation,
+		},
+		{
+			Name:  "TFO_GENERATION_PATH",
+			Value: generationPath,
+		},
+		{
+			Name:  "TFO_MAIN_MODULE",
+			Value: generationPath + "/main",
+		},
+		{
+			Name:  "TFO_TERRAFORM_VERSION",
+			Value: tf.Spec.TerraformVersion,
+		},
+	}...)
+
+	volumes := []corev1.Volume{
+		{
+			Name: "tfohome",
+			VolumeSource: corev1.VolumeSource{
+				PersistentVolumeClaim: &corev1.PersistentVolumeClaimVolumeSource{
+					ClaimName: tf.Status.PodNamePrefix,
+					ReadOnly:  false,
+				},
+			},
+		},
+	}
+	volumeMounts := []corev1.VolumeMount{
+		{
+			Name:      "tfohome",
+			MountPath: "/home/tfo-runner",
+			ReadOnly:  false,
+		},
+	}
+	env = append(env, corev1.EnvVar{
+		Name:  "TFO_ROOT_PATH",
+		Value: "/home/tfo-runner",
+	})
+
+	optional := true
+	xmode := int32(0775)
+	volumes = append(volumes, corev1.Volume{
+		Name: "gitaskpass",
+		VolumeSource: corev1.VolumeSource{
+			Secret: &corev1.SecretVolumeSource{
+				SecretName: versionedName,
+				Optional:   &optional,
+				Items: []corev1.KeyToPath{
+					{
+						Key:  "gitAskpass",
+						Path: "GIT_ASKPASS",
+						Mode: &xmode,
+					},
+				},
+			},
+		},
+	})
+	volumeMounts = append(volumeMounts, []corev1.VolumeMount{
+		{
+			Name:      "gitaskpass",
+			MountPath: "/git/askpass",
+		},
+	}...)
+	env = append(env, []corev1.EnvVar{
+		{
+			Name:  "GIT_ASKPASS",
+			Value: "/git/askpass/GIT_ASKPASS",
+		},
+	}...)
+
+	for _, c := range tf.Spec.Credentials {
+		if c.AWSCredentials.KIAM != "" {
+			annotations["iam.amazonaws.com/role"] = c.AWSCredentials.KIAM
+		}
+	}
+
+	for _, c := range tf.Spec.Credentials {
+		if (tfv1beta1.SecretNameRef{}) != c.SecretNameRef {
+			envFrom = append(envFrom, []corev1.EnvFromSource{
+				{
+					SecretRef: &corev1.SecretEnvSource{
+						LocalObjectReference: corev1.LocalObjectReference{
+							Name: c.SecretNameRef.Name,
+						},
+					},
+				},
+			}...)
+		}
+	}
+
+	labels["terraforms.tf.galleybytes.com/generation"] = generation
+	labels["terraforms.tf.galleybytes.com/resourceName"] = tf.Name
+	labels["terraforms.tf.galleybytes.com/podPrefix"] = tf.Status.PodNamePrefix
+	labels["terraforms.tf.galleybytes.com/terraformVersion"] = tf.Spec.TerraformVersion
+	labels["app.kubernetes.io/name"] = "terraform-operator"
+	labels["app.kubernetes.io/component"] = "terraform-operator-cli"
+	labels["app.kubernetes.io/instance"] = "debug"
+	labels["app.kubernetes.io/created-by"] = "cli"
+
+	initContainers := []corev1.Container{}
+	containers := []corev1.Container{}
+
+	// Make sure to use the same uid for containers so the dir in the
+	// PersistentVolume have the correct permissions for the user
+	user := int64(0)
+	group := int64(2000)
+	runAsNonRoot := false
+	privileged := true
+	allowPrivilegeEscalation := true
+	seLinuxOptions := corev1.SELinuxOptions{}
+	securityContext := &corev1.SecurityContext{
+		RunAsUser:                &user,
+		RunAsGroup:               &group,
+		RunAsNonRoot:             &runAsNonRoot,
+		Privileged:               &privileged,
+		AllowPrivilegeEscalation: &allowPrivilegeEscalation,
+		SELinuxOptions:           &seLinuxOptions,
+	}
+	restartPolicy := corev1.RestartPolicyNever
+
+	containers = append(containers, corev1.Container{
+		SecurityContext: securityContext,
+		Name:            "debug",
+		Image:           "ghcr.io/galleybytes/terraform-operator-tftaskv1.1.0:" + terraformVersion,
+		Command: []string{
+			"/bin/sleep", "86400",
+		},
+		ImagePullPolicy: corev1.PullIfNotPresent,
+		EnvFrom:         envFrom,
+		Env:             env,
+		VolumeMounts:    volumeMounts,
+	})
+
+	podSecurityContext := corev1.PodSecurityContext{
+		FSGroup: &group,
+	}
+	serviceAccount := tf.Spec.ServiceAccount
+	if serviceAccount == "" {
+		// By prefixing the service account with "tf-", IRSA roles can use wildcard
+		// "tf-*" service account for AWS credentials.
+		serviceAccount = "tf-" + versionedName
+	}
+
+	pod := &corev1.Pod{
+		ObjectMeta: metav1.ObjectMeta{
+			GenerateName: generateName,
+			Namespace:    tf.Namespace,
+			Labels:       labels,
+			Annotations:  annotations,
+		},
+		Spec: corev1.PodSpec{
+			SecurityContext:    &podSecurityContext,
+			ServiceAccountName: serviceAccount,
+			RestartPolicy:      restartPolicy,
+			InitContainers:     initContainers,
+			Containers:         containers,
+			Volumes:            volumes,
+		},
+	}
+
+	return pod
 }

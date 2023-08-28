bec76ceae6fba2cf462dd03af1f681980747c6de
isaaguilar <isa@aguilarstory.com>
2023-08-28 00:33:14 -0400

    add sso authentication

--- cmd/main.go
+++ cmd/main.go
@@ -5,12 +5,14 @@ import (
 	"io"
 	"log"
 	"os"
+	"strings"
 
 	"github.com/galleybytes/terraform-operator-api/internal/qworker"
 	"github.com/galleybytes/terraform-operator-api/pkg/api"
 	"github.com/galleybytes/terraform-operator-api/pkg/common/db"
 	tfv1beta1 "github.com/galleybytes/terraform-operator/pkg/apis/tf/v1beta1"
 	"github.com/gammazero/deque"
+	"github.com/spf13/pflag"
 	"github.com/spf13/viper"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
@@ -18,19 +20,55 @@ import (
 	"k8s.io/klog/v2"
 )
 
-func main() {
-	klog.InitFlags(flag.CommandLine)
-	flag.Parse()
-	flag.Set("logtostderr", "false")
-	flag.Set("alsologtostderr", "false")
-	klog.SetOutput(io.Discard)
+var (
+	port            string
+	dbUrl           string
+	ssoLoginURL     string
+	samlIssuer      string
+	samlRecipient   string
+	samlMetadataURL string
+)
 
+func main() {
 	viper.SetConfigFile("./pkg/common/envs/.env")
 	viper.ReadInConfig()
+	viper.SetEnvKeyReplacer(strings.NewReplacer("-", "_"))
 	viper.AutomaticEnv()
 
-	port := viper.Get("PORT").(string)
-	dbUrl := viper.Get("DB_URL").(string)
+	klog.InitFlags(nil)
+
+	pflag.CommandLine.AddGoFlag(flag.CommandLine.Lookup("v"))
+	pflag.CommandLine.AddGoFlag(flag.CommandLine.Lookup("logtostderr"))
+	pflag.CommandLine.Set("logtostderr", "true")
+	pflag.StringVar(&port, "port", "", "Port to expose the API on")
+	viper.BindPFlag("port", pflag.Lookup("port"))
+	pflag.StringVar(&dbUrl, "db-url", "", "Database url format (Example: 'postgres://user:password@srv:5432/db')")
+	viper.BindPFlag("db-url", pflag.Lookup("db-url"))
+	pflag.StringVar(&ssoLoginURL, "sso-login-url", "", "IDP Login URL ")
+	viper.BindPFlag("sso-login-url", pflag.Lookup("sso-login-url"))
+	pflag.StringVar(&samlIssuer, "saml-issuer", "", "Identity Provider (IDP) Issuer")
+	viper.BindPFlag("saml-issuer", pflag.Lookup("saml-issuer"))
+	pflag.StringVar(&samlRecipient, "saml-recipient", "", "Service Provider")
+	viper.BindPFlag("saml-recipient", pflag.Lookup("saml-recipient"))
+	pflag.StringVar(&samlMetadataURL, "saml-metadata-url", "", "IDP Metadata URL")
+	viper.BindPFlag("saml-metadata-url", pflag.Lookup("saml-metadata-url"))
+	pflag.Parse()
+
+	pflag.Set("alsologtostderr", "false")
+	pflag.Set("logtostderr", "false")
+
+	// goflag.Parse()
+	klog.SetOutput(io.Discard)
+
+	klog.Info("Don't show this log")
+	klog.Warning("Don't show this warning")
+
+	port = viper.GetString("port")
+	dbUrl = viper.GetString("db-url")
+	ssoLoginURL = viper.GetString("sso-login-url")
+	samlIssuer = viper.GetString("saml-issuer")
+	samlRecipient = viper.GetString("saml-recipient")
+	samlMetadataURL = viper.GetString("saml-metadata-url")
 
 	clientset := kubernetes.NewForConfigOrDie(NewConfigOrDie(os.Getenv("KUBECONFIG")))
 	database := db.Init(dbUrl)
@@ -38,7 +76,15 @@ func main() {
 
 	qworker.BackgroundWorker(&queue)
 
-	apiHandler := api.NewAPIHandler(database, &queue, clientset)
+	ssoConfig, err := api.NewSAMLConfig(samlIssuer, samlRecipient, samlMetadataURL)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if ssoConfig != nil {
+		ssoConfig.URL = ssoLoginURL
+	}
+
+	apiHandler := api.NewAPIHandler(database, &queue, clientset, ssoConfig)
 	apiHandler.RegisterRoutes()
 	apiHandler.Server.Run(port)
 }
--- pkg/api/auth.go
+++ pkg/api/auth.go
@@ -1,15 +1,28 @@
 package api
 
 import (
+	"bytes"
+	"context"
+	"crypto/tls"
+	"crypto/x509"
+	_ "embed"
+	"encoding/base64"
 	"fmt"
+	"html/template"
 	"net/http"
+	"net/url"
 	"os"
 	"time"
 
+	"github.com/crewjam/saml/samlsp"
 	"github.com/gin-gonic/gin"
 	"github.com/golang-jwt/jwt/v4"
+	"github.com/ucarion/saml"
 	"golang.org/x/crypto/bcrypt"
 )
+
+//go:embed manifests/samlconnecter.html
+var samlConnecterHTMLTemplate string
 
 var jwtSigningKey string = os.Getenv("JWT_SIGNING_KEY")
 
@@ -90,4 +103,105 @@ func generateJWT(username string) (string, error) {
 		return "", fmt.Errorf("something went wrong: %s", err.Error())
 	}
 	return tokenString, nil
+}
+
+func (h APIHandler) defaultConnectMethod(c *gin.Context) {
+	if h.ssoConfig != nil {
+		c.JSON(http.StatusOK, response(http.StatusOK, "", []string{"sso"}))
+		return
+	} else {
+		c.JSON(http.StatusOK, response(http.StatusOK, "", []string{"basic"}))
+		return
+	}
+
+}
+
+func (h APIHandler) ssoRedirecter(c *gin.Context) {
+	if h.ssoConfig == nil {
+		c.AbortWithError(http.StatusNotAcceptable, fmt.Errorf("no SAML configuration found on server"))
+		return
+	}
+	c.Redirect(http.StatusMovedPermanently, h.ssoConfig.URL)
+}
+
+func (h APIHandler) samlConnecter(c *gin.Context) {
+	if h.ssoConfig == nil {
+		c.AbortWithError(http.StatusNotAcceptable, fmt.Errorf("no SAML configuration found on server"))
+		return
+	}
+	if h.ssoConfig.saml == nil {
+		c.AbortWithError(http.StatusNotAcceptable, fmt.Errorf("no SAML configuration found on server"))
+		return
+	}
+
+	err := c.Request.ParseForm()
+	if err != nil {
+		c.AbortWithError(http.StatusNotAcceptable, err)
+	}
+
+	data := c.Request.Form
+	samlResponseData := data.Get("SAMLResponse")
+
+	samlResponse, err := saml.Verify(
+		samlResponseData,
+		h.ssoConfig.saml.issuer,
+		h.ssoConfig.saml.crt,
+		h.ssoConfig.saml.recipient,
+		time.Now(),
+	)
+
+	if err != nil {
+		c.AbortWithError(http.StatusNotAcceptable, err)
+		return
+	}
+	username := samlResponse.Assertion.Subject.NameID.Value
+	if err != nil {
+		c.AbortWithError(http.StatusNotAcceptable, fmt.Errorf("username not found"))
+		return
+	}
+
+	jwtToken, err := generateJWT(username)
+	if err != nil {
+		c.AbortWithError(http.StatusNotAcceptable, err)
+		return
+	}
+
+	buf := bytes.NewBuffer([]byte{})
+	tmpl, _ := template.New("").Parse(string(samlConnecterHTMLTemplate))
+	err = tmpl.Execute(buf, struct {
+		Token string `json:"token"`
+	}{
+		Token: jwtToken,
+	})
+	if err != nil {
+		c.AbortWithError(http.StatusNotAcceptable, err)
+		return
+	}
+
+	fmt.Fprintln(c.Writer, buf.String())
+}
+
+func fetchIDPCertificate(metadataURL string) (*x509.Certificate, error) {
+	url, err := url.Parse(metadataURL)
+	if err != nil {
+		panic(err)
+	}
+
+	httpClient := http.Client{
+		Transport: &http.Transport{
+			TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+		},
+	}
+	entityDescriptor, err := samlsp.FetchMetadata(context.Background(), &httpClient, *url)
+	if err != nil {
+		panic(err)
+	}
+
+	crt := entityDescriptor.IDPSSODescriptors[0].KeyDescriptors[0].KeyInfo.X509Data.X509Certificates[0].Data
+	b, err := base64.StdEncoding.DecodeString(crt)
+	if err != nil {
+		return nil, err
+	}
+	return x509.ParseCertificate(b)
+
 }
